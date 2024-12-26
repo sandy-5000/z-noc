286e4034db79e56dee046f0f29d6bcc2ef7e1826
Sandeep Kumar Bhaviri <112413132+sandy-5000@users.noreply.github.com>
2024-12-26 22:32:42 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,45 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 #define endl "\n"
 #define int long long
 #define mod_ 1000000007
+#define at_index find_by_order
+#define index_of order_of_key
 #define fastIO                    \
     ios_base::sync_with_stdio(0); \
     cin.tie(0);                   \
     cout.tie(0);
 using namespace std;
+using namespace __gnu_pbds;
+template <typename T> using ordered_set = tree<
+    T,
+    null_type,
+    less<T>,
+    rb_tree_tag,
+    tree_order_statistics_node_update
+>;
 
 
 void pixel() {
-    cout << "Hello World" << endl;
+    cout << "Hello World!" << endl;
 }
+
 
 int32_t main() {
     fastIO;
     int t = 1;
     cin >> t;
     while (t--) {
-        pixel();        
+        pixel();
     }
     return 0;
 }
 
 
 
-
-
 /*
- _____ _____ _____ _____ _____ 
+ _____ _____ _____ _____ _____
 |     |  _  |   __|   __|     |
 |   --|     |__   |__   |-   -|
 |_____|__|__|_____|_____|_____|
